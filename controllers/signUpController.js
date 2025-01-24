f1bd6c91d04dea825807d3cbfb9fb708373e4965
dee <178827694+dee797@users.noreply.github.com>
2025-01-24 13:03:29 -0700

    Validate username, password, and confirmPassword fields for post request

--- controllers/signUpController.js
+++ controllers/signUpController.js
@@ -12,15 +12,34 @@ const getSignUp = (req, res) => {
 
 // Sanitization / Validation
 
-const lengthErr = "must be between 8 and 50 characters.";
-
 const validate = [
-  body("username").trim()
-    .isLength({ min: 8, max: 50 }).withMessage(`Username ${lengthErr}`),
+  body("username")
+    .trim()
+    .isLength({ min: 8, max: 50 })
+    .withMessage(`Username must be between 8 and 50 characters.`)
+    .custom(async username => {
+      const user = await db.getUserByUsername(username);
+      if (user) { 
+        throw new Error("Username already in use.") 
+      }
+    }),
 
-    body("password").trim()
-    .isLength({ min: 8, max: 50 }).withMessage(`Password ${lengthErr}`),
+  body("password")
+    .isStrongPassword({
+      minLength: 8,
+      minLowercase: 1,
+      minUppercase: 1, 
+      minNumbers: 1,
+      minSymbols: 1,
+      returnScore: false
+    })
+    .withMessage(`Password does not meet the requirements listed above.`),
 
+  body("confirmPassword")
+    .custom((value, { req }) => {
+      return value === req.body.password;
+    })
+    .withMessage(`Passwords do not match.`)
 ];
 
 
@@ -28,6 +47,18 @@ const validate = [
 
 const postSignUp = [
     validate,
+
+    (req, res, next) => {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).render("signUpForm", {
+          errors: errors.mapped()
+        });
+      }
+      next();
+    },
+
     asyncHandler(async (req, res) => {
         const hashedPassword = await bcrypt.hash(req.body.password, 10);
         await db.addNewUser(req.body.username, hashedPassword);
