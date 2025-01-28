cb3f6e33105aaf23726c91abe655ec6021246f77
dee <178827694+dee797@users.noreply.github.com>
2025-01-28 15:33:14 -0700

    Destroy session and delete cookie from client on logout for security

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 // node_modules imports
-require('dotenv').config();
+require("dotenv").config();
 const path = require("node:path");
 const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
 const pgSession = require("connect-pg-simple")(session);
+const { isAuthenticated } = require("./controllers/authenticateController");
 
 
 // Router imports
-const signUpRouter = require('./routes/signUpRouter');
-const loginRouter = require('./routes/loginRouter');
-
+const signUpRouter = require("./routes/signUpRouter");
+const loginRouter = require("./routes/loginRouter");
 
 
 // Configurations
-
 const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,"public")));
 
 
 // Session Setup
@@ -28,12 +27,13 @@ app.use(session({
     pool: require("./db/pool")
   }),
   secret: process.env.SECRET, 
-  genid: () => require('crypto').randomBytes(32).toString('hex'),
+  genid: () => require("crypto").randomBytes(32).toString("hex"),
   resave: false, 
   saveUninitialized: false,
   cookie: { maxAge: 1000 * 60 * 60 * 24 },
-  unset: 'destroy'
+  unset: "destroy"
 }));
+
 
 //Passport Authentication
 app.use(passport.session());
@@ -47,51 +47,37 @@ app.use((req, res, next) => {
 app.use("/signup", signUpRouter);
 app.use("/login", loginRouter);
 
-app.get("/logout", (req, res, next) => {
+app.get("/logout", isAuthenticated, (req, res, next) => {
   req.logout((err) => {
     if (err) {
       return next(err);
     }
-    res.redirect("/");
+
+    res.clearCookie("connect.sid");
+    req.session.destroy((err) => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect("/");
+    });
   });
 });
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.render("index", { user: res.locals.currentUser });
 });
 
-/*
-app.get("/:id", async (req, res) => {
-  try {
-    const message = await getMessageById(req.params.id, app.locals.messages);
-
-    if (!message) {
-      res.status(404).render("error");
-      return;
-    }
-
-    res.render("message", {message: message});
-
-  } catch (err) {
-    console.error(err);
-    res.status(505).send("Internal Server Error");
-  }
-});
-*/
-
 
 // Error handling
-
 app.use((err, req, res, next) => {
   console.error(err);
   res.status(err.statusCode || 500).send(err.message || "Internal server error");
 });
 
-app.all('/*', (req, res, next) => {
+app.all("/*", (req, res, next) => {
   res.status(404).send("404 - Not found");
   ;
 });
-
 
 
 
