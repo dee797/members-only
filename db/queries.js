006c256329aef700054488dd64615f9fd3ad8aba
dee <178827694+dee797@users.noreply.github.com>
2025-01-28 17:36:53 -0700

    refactor: store functions in variables for consistency across app

--- db/queries.js
+++ db/queries.js
@@ -1,23 +1,23 @@
 const pool = require("./pool");
 
-async function getUserByUsername(username) {
+const getUserByUsername = async (username) => {
   const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
   return rows[0];
 }
 
-async function getUserByID(id) {
+const getUserByID = async (id) => {
   const { rows } = await pool.query("SELECT * FROM users WHERE userid = $1", [id]);
   return rows[0];
 }
 
-async function addNewUser(username, hashedPassword) {
+const addNewUser = async (username, hashedPassword) => {
   await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
     username,
     hashedPassword,
   ]);
 }
 
-async function addNewMessage(userid, message, title) {
+const addNewMessage = async (userid, message, title) => {
   await pool.query("INSERT INTO messages (message, title, userid) VALUES ($1, $2, $3)", [
     message,
     title,
@@ -25,8 +25,16 @@ async function addNewMessage(userid, message, title) {
   ]);
 }
 
-async function updateUserMembership(userid) {
+const updateUserMembership = async (userid) => {
   await pool.query("UPDATE users SET member = TRUE WHERE userid = $1", [userid]);
+}
+
+const getAllMessages = async () => {
+  const { rows } = await pool.query(`SELECT messages.message, messages.title, messages.messagetimestamp, users.username
+                                     FROM messages 
+                                     INNER JOIN users ON messages.userid = users.userid
+                                     ORDER BY messages.messagetimestamp`);
+  return rows;
 }
 
 
@@ -35,5 +43,6 @@ module.exports = {
     getUserByID,
     addNewUser,
     addNewMessage,
-    updateUserMembership
+    updateUserMembership,
+    getAllMessages
 }
